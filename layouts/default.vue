ed8e85f901f1f4e92fb075e2a4150b38dca76345
CaetanoLeal <caetanolaraleal@gmail.com>
2023-12-23 23:57:09 -0300

    oi

---
+++ layouts/default.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-app dark>
+    <v-row>
+      <v-column>
+        <v-checkbox label="O Inglês vive na casa Vermelha." />
+        <v-checkbox label="O Sueco tem Cachorros como animais de estimação." />
+        <v-checkbox label="O Dinamarquês bebe Chá." />
+        <v-checkbox label="A casa Verde fica do lado esquerdo da casa Branca." />
+        <v-checkbox label="O homem que vive na casa Verde bebe Café." />
+        <v-checkbox label="O homem que fuma Pall Mall cria Pássaros." />
+        <v-checkbox label="O homem que vive na casa Amarela fuma Dunhill." />
+        <v-checkbox label="O homem que vive na casa do meio bebe Leite." />
+      </v-column>
+      <v-column>
+        <v-checkbox label="O Norueguês vive na primeira casa." />
+        <v-checkbox label="O homem que fuma Blends vive ao lado do que tem Gatos." />
+        <v-checkbox label="O homem que cria Cavalos vive ao lado do que fuma Dunhill." />
+        <v-checkbox label="O homem que fuma Blue Master bebe Cerveja." />
+        <v-checkbox label="O Alemão fuma Prince." />
+        <v-checkbox label="O Norueguês vive ao lado da casa Azul." />
+        <v-checkbox label="O homem que fuma Blends é vizinho do que bebe Água." />
+      </v-column>
+    </v-row>
+  </v-app>
+</template>
